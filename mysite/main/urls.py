ee6fa78239bbb68ad8190eb17519bf1470641ae7
Endenissanse <dov.raven@gmail.com>
2023-12-31 16:14:56 +0900

    added path views/

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views #import views from current directory
 
 urlpatterns = [
-    path('', views.index(), name='index'), #if go to adress without any '/' after '.com'
-                                        # redirect you to '.com' + 'index' and return a func 'index' from views.py
+    path('', views.index, name='index'), #if go to adress without any '/' after '.com'
+                                         # redirect you to '.com' + 'index' and return a func 'index' from views.py
+    path('views/', views.v1, name='view 1'),
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')), # if web-path is blank, redirect to file 'urls.py' in folder 'main' to provide response
+    path('views/', include('main.urls')),
 ]
