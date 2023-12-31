486e7389113ba6db77ee2fbcd7b15c8de6f4e827
Endenissanse <dov.raven@gmail.com>
2023-12-31 16:02:19 +0900

    added comment to path ''

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views #import views from current directory
 
 urlpatterns = [
-    path('', views.index(), name='index'),
+    path('', views.index(), name='index'), #if go to adress without any '/' after '.com'
+                                        # redirect you to '.com' + 'index' and return a func 'index' from views.py
 ]
