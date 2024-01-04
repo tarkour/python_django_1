2d930a0cbf0e5e0d8a33d73a31057cb0c1e28fbb
Endenissanse <dov.raven@gmail.com>
2024-01-04 21:06:25 +0900

    added 'base' and 'home' templates +path and func to access to them

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,5 +6,5 @@ from . import views #import views from current directory
 urlpatterns = [
     path('<str:name>', views.index, name='index'), #if go to adress without any '/' after '.com'
                                          # redirect you to '.com' + 'index' and return a func 'index' from views.py
-
+    path('', views.home, name='home')
 ]
