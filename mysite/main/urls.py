4628520e0df0d14dbcc6c36f74a8871f25512df4
Endenissanse <dov.raven@gmail.com>
2024-01-04 16:06:43 +0900

    added opportunity to observe 'name' and 'text' of ToDoList by typing name of list into web-path

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views #import views from current directory
 
 urlpatterns = [
-    path('', views.index, name='index'), #if go to adress without any '/' after '.com'
+    path('<str:name>', views.index, name='index'), #if go to adress without any '/' after '.com'
                                          # redirect you to '.com' + 'index' and return a func 'index' from views.py
 
 ]
